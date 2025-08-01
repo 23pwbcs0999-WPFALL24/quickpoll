4196377ce53acbe346f8ef464516ab8a0658d83e
23pwbcs0999-WPFALL24 <23pwbcs0999@uetpeshawar.edu.pk>
2025-08-01 15:16:39 +0500

    Commit

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,4 +1,4 @@
-// filepath: c:\Users\user\Desktop\quickpoll\client\src\components\Footer.js
+// filepath: src/components/Footer.js
 import React from 'react';
 import './Footer.css';
 
@@ -6,17 +6,16 @@ export default function Footer() {
   return (
     <footer className="app-footer">
       <div className="footer-content">
-        <div className="footer-brand">QuickPoll &copy; 2025</div>
+        <div className="footer-brand">© 2025 QuickPoll. All rights reserved.</div>
         <div className="footer-social">
           <a href="https://twitter.com/" target="_blank" rel="noopener noreferrer" aria-label="Twitter">
-            <svg width="24" height="24" fill="currentColor" viewBox="0 0 24 24"><path d="M22.46 6c-.77.35-1.6.59-2.47.7a4.3 4.3 0 0 0 1.88-2.37 8.59 8.59 0 0 1-2.72 1.04A4.28 4.28 0 0 0 16.11 4c-2.37 0-4.29 1.92-4.29 4.29 0 .34.04.67.11.99C7.69 9.13 4.07 7.38 1.64 4.77c-.37.64-.58 1.39-.58 2.19 0 1.51.77 2.84 1.95 3.62-.72-.02-1.39-.22-1.98-.55v.06c0 2.11 1.5 3.87 3.5 4.27-.36.1-.74.16-1.13.16-.28 0-.54-.03-.8-.08.54 1.68 2.11 2.9 3.97 2.93A8.6 8.6 0 0 1 2 19.54c-.28 0-.56-.02-.83-.05A12.13 12.13 0 0 0 8.29 21.5c7.55 0 11.68-6.26 11.68-11.68 0-.18-.01-.36-.02-.54A8.18 8.18 0 0 0 24 4.59a8.36 8.36 0 0 1-2.54.7z"/></svg>
+            <i className="fab fa-twitter" />
           </a>
           <a href="https://github.com/" target="_blank" rel="noopener noreferrer" aria-label="GitHub">
-            <svg width="24" height="24" fill="currentColor" viewBox="0 0 24 24"><path d="M12 2C6.48 2 2 6.58 2 12.26c0 4.49 2.87 8.3 6.84 9.64.5.09.68-.22.68-.48 0-.24-.01-.87-.01-1.7-2.78.62-3.37-1.36-3.37-1.36-.45-1.18-1.1-1.5-1.1-1.5-.9-.63.07-.62.07-.62 1 .07 1.53 1.05 1.53 1.05.89 1.56 2.34 1.11 2.91.85.09-.66.35-1.11.63-1.37-2.22-.26-4.56-1.14-4.56-5.07 0-1.12.39-2.03 1.03-2.75-.1-.26-.45-1.3.1-2.7 0 0 .84-.28 2.75 1.05A9.38 9.38 0 0 1 12 7.43c.85.004 1.71.12 2.51.35 1.91-1.33 2.75-1.05 2.75-1.05.55 1.4.2 2.44.1 2.7.64.72 1.03 1.63 1.03 2.75 0 3.94-2.34 4.81-4.57 5.07.36.32.68.94.68 1.9 0 1.37-.01 2.47-.01 2.81 0 .27.18.58.69.48A10.01 10.01 0 0 0 22 12.26C22 6.58 17.52 2 12 2z"/></svg>
+            <i className="fab fa-github" />
           </a>
         </div>
       </div>
     </footer>
   );
 }
-//footer
