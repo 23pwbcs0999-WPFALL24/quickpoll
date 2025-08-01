3dfd19e48d818c3c56bbd4b5a6f9c13a7d773a78
23pwbcs0999-WPFALL24 <23pwbcs0999@uetpeshawar.edu.pk>
2025-08-01 17:00:18 +0500

    Commit

--- server/utils/tokenGenerator.js
+++ server/utils/tokenGenerator.js
@@ -1,8 +1,7 @@
-// helpers/tokenGenerator.js
 const { v4: uuidv4 } = require('uuid');
 
 function generateTokens(count) {
-  return Array.from({ length: count }, () => uuidv4());
+  return Array.from({ length: count }, () => uuidv4().slice(0, 8)); // Only first 8 chars
 }
 
 module.exports = { generateTokens };
